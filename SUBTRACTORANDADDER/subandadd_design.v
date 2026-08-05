c2e27f819fc593464d1c99269d48559c7bfc576c
kevinlyons20 <kevinlyonss20@gmail.com>
2026-08-05 11:43:37 +0530

    Update subandadd_design.v

--- SUBTRACTORANDADDER/subandadd_design.v
+++ SUBTRACTORANDADDER/subandadd_design.v
@@ -1,13 +1,13 @@
 module addandsub(
     input  [3:0]a,b,
-    input  c,d,
+    input  d,
     output reg [3:0]sd,
     output reg e
 );
 always@(*) begin
     if(d==1) 
-        {e,sd}=a+b+c;
+        {e,sd}=a+b;
     else
-        {e,sd}=a+(~b)+1;
+        {e,sd}=a-b;
     end
 endmodule
