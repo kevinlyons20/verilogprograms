80917ccde5245cedca1fb668591f4c7d7d965899
kevinlyons20 <kevinlyonss20@gmail.com>
2026-08-12 07:29:23 +0530

    Create encoder_16:4_tb.v

---
+++ ENCODER_16:4/encoder_16:4_tb.v
@@ -0,0 +1,16 @@
+module encoder_16_4_tb;
+reg [15:0]a;
+wire[3:0]y;
+encoder_16_4  dut(.a(a),.y(y));
+integer i;
+initial begin
+    $dumpfile("encoder_16_4.vcd");
+    $dumpvars(0,dut);
+    $monitor("a=%b,y=%b",a,y);
+    for(i=0;i<16;i=i+1) begin
+        a={16'b0000000000000001 << i};
+        #5;
+    end
+    $finish;
+end
+endmodule
