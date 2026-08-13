b596ba1dfea94b7d89300e0e02c6f50da4f0bcb9
KEVIN LYONS <kevinlyonss20@gmail.com>
2026-08-13 14:02:57 +0530

    Create decoder_4:2_design.v

---
+++ DECODER_4:2/decoder_4:2_design.v
@@ -0,0 +1,16 @@
+module decoder(
+  input [1:0]a,
+  output reg [3:0]y
+);
+  integer i;
+  always@(*) begin
+    y=2'b00;
+    case(a)
+      2'b00: y=4'b0001;
+      2'b01: y=4'b0010;
+      2'b10: y=4'b0100;
+      2'b11: y=4'b1000;
+    endcase
+    end
+endmodule
+  
