9bccc322c4223b1469230adbb1ab7e8e3f4178ed
KEVIN LYONS <kevinlyonss20@gmail.com>
2026-08-13 13:48:58 +0530

    Create encoder_32:5_tb.v

---
+++ ENCODER_32:5/encoder_32:5_tb.v
@@ -0,0 +1,16 @@
+module encoder_32_5_tb;
+reg [31:0]a;
+wire[4:0]y;
+encoder_32_5  dut(.a(a),.y(y));
+integer i;
+initial begin
+    $dumpfile("encoder_32_5.vcd");
+    $dumpvars(0,dut);
+    $monitor("a=%b,y=%b",a,y);
+    for(i=0;i<32;i=i+1) begin
+        a={32'd1 << i};
+        #5;
+    end
+    $finish;
+end
+endmodule
