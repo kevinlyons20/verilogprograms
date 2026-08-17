4640c3ac25cb6ce9ed1c883355adba1f4b652ee0
KEVIN LYONS <kevinlyonss20@gmail.com>
2026-08-17 11:58:03 +0530

    Create priority_enc_design.v

---
+++ PRIORITY_ENCODER_4:2/priority_enc_design.v
@@ -0,0 +1,15 @@
+module encoder(
+  input [3:0]a,
+  output reg [1:0]y
+);
+  always@(*) begin
+    if(a[3])
+      y=2'b11;
+    else if(a[2])
+      y=2'b10;
+    else if(a[1])
+      y=2'b01;
+    else 
+      y=2'b00;
+  end
+endmodule
