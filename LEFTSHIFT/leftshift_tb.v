3d53ef716dec00b002b924e4831a39d34d82ceff
KEVIN LYONS <kevinlyonss20@gmail.com>
2026-08-20 12:48:14 +0530

    Create leftshift_tb.v

---
+++ LEFTSHIFT/leftshift_tb.v
@@ -0,0 +1,11 @@
+module logicalshift_tb;
+  reg [7:0]a;
+  wire [7:0]b;
+  logicalleftshift dut(.a(a),.b(b));
+  initial begin
+    $monitor("a=%b b=%b",a,b);
+    a=8'b10101010;
+    #5;
+    $finish;
+  end
+endmodule
