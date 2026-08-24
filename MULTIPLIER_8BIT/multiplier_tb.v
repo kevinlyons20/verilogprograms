69f4e22f54b8d2e9e020e871d9461f6e40d4c43f
KEVIN LYONS <kevinlyonss20@gmail.com>
2026-08-24 13:35:13 +0530

    Create multiplier_tb.v

---
+++ MULTIPLIER_8BIT/multiplier_tb.v
@@ -0,0 +1,13 @@
+module multi_tb;
+reg [7:0]a,b;
+wire [15:0]y;
+multi dut (.a(a),.b(b),.y(y));
+initial begin
+    $dumpfile("multi.vcd");
+    $dumpvars(0,dut);
+    $monitor("a=%b b=%b y=%b",a,b,y);
+    a=8'b10101010;b=8'b00000010;
+    #5;
+    $finish;
+end
+endmodule
