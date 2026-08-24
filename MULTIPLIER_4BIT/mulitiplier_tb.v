75e161c99bf460a47e0450ce386848a1020a43e0
KEVIN LYONS <kevinlyonss20@gmail.com>
2026-08-24 13:33:33 +0530

    Create mulitiplier_tb.v

---
+++ MULTIPLIER_4BIT/mulitiplier_tb.v
@@ -0,0 +1,13 @@
+module multi_tb;
+reg [3:0]a,b;
+wire [7:0]y;
+multi dut (.a(a),.b(b),.y(y));
+initial begin
+    $dumpfile("multi.vcd");
+    $dumpvars(0,dut);
+    $monitor("a=%b b=%b y=%b",a,b,y);
+    a=4'b1010;b=4'b0010;
+    #5;
+    $finish;
+end
+endmodule
