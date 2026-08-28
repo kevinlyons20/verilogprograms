a3303851686aac97cc3d9b51b66566d03ff3f6d6
KEVIN LYONS <kevinlyonss20@gmail.com>
2026-08-28 19:34:54 +0530

    Create trailzerodet_design.v

---
+++ TRAILING_ZERODETECTOR/trailzerodet_design.v
@@ -0,0 +1,19 @@
+module trailing_zero_detector(
+    input  [7:0] a,
+    output reg [3:0] count
+);
+integer i;
+reg found;
+always @(*) begin
+    count = 0;
+    found = 0;
+    for(i = 0; i < 8; i = i + 1) begin
+        if(!found && a[i] == 1'b1) begin
+            count = i;
+            found = 1;
+        end
+    end
+    if(a == 8'b00000000)
+        count = 8;
+end
+endmodule
