dd6e0ff64875da91de3b2789ffc34db2413b54a9
KEVIN LYONS <kevinlyonss20@gmail.com>
2026-09-09 11:45:18 +0530

    Create DFFwithasynchrst_design.v

---
+++ D FLIPFLOP WITH ASYNCHRONOUS RESET/DFFwithasynchrst_design.v
@@ -0,0 +1,11 @@
+module dflip_async_reset(
+    input d,clk,reset,
+    output reg q
+);
+always @(posedge clk or posedge reset) begin
+    if (reset)
+        q <= 0;
+    else
+        q <= d;
+end
+endmodule
