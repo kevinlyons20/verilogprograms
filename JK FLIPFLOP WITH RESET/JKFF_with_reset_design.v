5e46a17ef711b84a071021707505257ef94169fa
KEVIN LYONS <kevinlyonss20@gmail.com>
2026-09-10 11:28:23 +0530

    Create JKFF_with_reset_design.v

---
+++ JK FLIPFLOP WITH RESET/JKFF_with_reset_design.v
@@ -0,0 +1,16 @@
+module jkff_with_reset(
+    input j,k,clk,rst,
+    output reg q
+);
+always@(posedge clk or posedge rst) begin
+    if(rst) 
+        q<=0;
+    else
+        case({j,k})
+            2'b00: q<=q;
+            2'b01: q<=0;
+            2'b10: q<=1;
+            2'b11: q<=~q;
+        endcase
+end
+endmodule
