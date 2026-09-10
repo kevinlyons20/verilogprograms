853b0e804f6143fbecbf9f30da1e74691a04d5f4
KEVIN LYONS <kevinlyonss20@gmail.com>
2026-09-10 11:30:54 +0530

    Create JKFF_with_enable_design.v

---
+++ JK FLIPFLOP WITH ENABLE/JKFF_with_enable_design.v
@@ -0,0 +1,14 @@
+module jkff_with_enable(
+    input j,k,clk,en,
+    output reg q
+);
+always@(posedge clk) begin
+    if(en)
+        case({j,k})
+            2'b00: q<=q;
+            2'b01: q<=0;
+            2'b10: q<=1;
+            2'b11: q<=~q;
+        endcase
+end
+endmodule
