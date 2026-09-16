de51148acd13df8876925986c64312aeac8a0de6
KEVIN LYONS <kevinlyonss20@gmail.com>
2026-09-16 10:36:12 +0530

    Create regwith_asyn_rst_design.v

---
+++ REGISTER WITH ASYNCHRONOUS RESET/regwith_asyn_rst_design.v
@@ -0,0 +1,14 @@
+module register_async_reset(
+    input clk,
+    input reset,
+    input [3:0] d,
+    output reg [3:0] q
+);
+always @(posedge clk or posedge reset)
+begin
+    if (reset)
+        q <= 4'b0000;
+    else
+        q <= d;
+end
+endmodule
