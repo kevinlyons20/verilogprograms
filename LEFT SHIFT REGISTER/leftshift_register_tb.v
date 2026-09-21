75d12da6fe5edcfdc1fdebda37712ca522cf9427
KEVIN LYONS <kevinlyonss20@gmail.com>
2026-09-21 18:48:10 +0530

    Create leftshift_register_tb.v

---
+++ LEFT SHIFT REGISTER/leftshift_register_tb.v
@@ -0,0 +1,18 @@
+module leftshiftreg_tb;
+reg d,clk;
+wire [3:0]q;
+leftshiftreg dut(.d(d),.clk(clk),.q(q));
+always #5 clk=~clk;
+initial begin
+    $dumpfile("shiftreg.vcd");
+    $dumpvars(0,dut);
+    $monitor("clk=%b d=%b q=%b",clk,d,q);
+    d=1;clk=0;
+    #10;d=0;
+    #10;d=0;
+    #10;d=1;
+    #10;d=1;
+    #10;d=0;
+    #10;$finish;
+end
+endmodule
