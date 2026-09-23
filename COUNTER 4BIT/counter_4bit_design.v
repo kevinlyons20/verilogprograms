0cd044acabcaa792c13c180a5fb422908089f734
KEVIN LYONS <kevinlyonss20@gmail.com>
2026-09-23 09:36:09 +0530

    Create counter_4bit_design.v

---
+++ COUNTER 4BIT/counter_4bit_design.v
@@ -0,0 +1,12 @@
+module counter_4bit(
+    input clk,rst,
+    output reg [3:0]q
+);
+initial q=4'd0;
+always@(posedge clk or posedge rst) begin
+    if(rst)
+        q<=4'd0;
+    else
+        q<=q+1;
+end
+endmodule
