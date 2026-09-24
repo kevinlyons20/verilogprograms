b7e161516ef2bfe59ad7758696f73e2d5ef41088
KEVIN LYONS <kevinlyonss20@gmail.com>
2026-09-24 11:57:08 +0530

    Create upcounter_4bit_tb.v

---
+++ UPCOUNTER 4BIT/upcounter_4bit_tb.v
@@ -0,0 +1,16 @@
+module upcounter_4bit_tb;
+reg clk,rst;
+wire [3:0]q;
+upcounter_4bit dut(.clk(clk),.rst(rst),.q(q));
+always #5 clk=~clk;
+always@(posedge clk) begin
+    $display("time=%0t  clk=%b rst=%b q=%d",$time,clk,rst,q);
+end
+initial begin
+    $dumpfile("upcounter2bit.vcd");
+    $dumpvars(0,dut);
+    clk=0;rst=1;
+    #5;rst=0;
+    #150;$finish;
+end
+endmodule
